98527a9c37db86a4a4c2aa537409ba6eab60527b
Rakesh Yadav <yadav94rakesh@gmail.com>
2023-06-20 12:01:49 +0530

    create E-com

---
+++ src/App.js
@@ -0,0 +1,43 @@
+const App = () => {
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Curtain</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Lamp</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Rods</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Hooks</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Cusion</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
